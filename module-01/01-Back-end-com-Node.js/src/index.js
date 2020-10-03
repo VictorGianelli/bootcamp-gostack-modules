cf7659305b33235a9792b6d58d617af12c8c7e33
Victor Gianelli <victorgnll@gmail.com>
2020-10-03 17:21:04 -0300

    Utilizando o Insomnia

--- module-01/01-Back-end-com-Node.js/src/index.js
+++ module-01/01-Back-end-com-Node.js/src/index.js
@@ -1,9 +1,44 @@
-const express = require('express');
+const express = require('express');//06:37 
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ Mensagem : 'Hello World!'});
+/**
+ * Métodos HTTP:
+ * 
+ * GET: Buscar informações do back-end
+ * POST: Criar uma informação no back-end
+ * PUT/PATCH: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ */
+
+app.get('/projects', (request, response) => {
+  return response.json([
+    'Projeto 1',
+    'Projeto 2'
+  ]);
+});
+
+app.post('/projects', (request, response) => {
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+    'Projeto 3'
+  ]);
+});
+
+app.put('/projects/:id', (request, response) => {
+  return response.json([
+    'Projeto 4',
+    'Projeto 2',
+    'Projeto 3'
+  ]);
+});
+
+app.delete('/projects/:id', (request, response) => {
+  return response.json([
+    'Projeto 2',
+    'Projeto 3'
+  ]);
 });
 
 
