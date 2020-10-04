95570c4b2cdd500f6be3eddb8f36aa1df605a8f3
Victor Gianelli <victorgnll@gmail.com>
2020-10-03 21:02:20 -0300

    Middlewares

--- module-01/01-Back-end-com-Node.js/src/index.js
+++ module-01/01-Back-end-com-Node.js/src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');//10:14 
-const { uuid } = require('uuidv4');//10:14 
+const { uuid, isUuid } = require('uuidv4');//10:14 
 
 const app = express();
 
@@ -15,14 +15,45 @@ app.use(express.json());
  */
 
 /**
-* Tipos de parâmetros:
-* 
-* Query Params: Filtros e paginação
-* Route Params: Identificar recursos (Atualizar/Deletar)
-* Request Body: Conteúdo na hora de criar ou editar um recurso (JSON)
-*/
+ * Tipos de parâmetros:
+ * 
+ * Query Params: Filtros e paginação
+ * Route Params: Identificar recursos (Atualizar/Deletar)
+ * Request Body: Conteúdo na hora de criar ou editar um recurso (JSON)
+ */
+
+/**
+ * Middleware:
+ * 
+ * Interceptador de requisições que pode interromper totalmente a requisição ou alterar dados da requisição.
+ */
 
 const projects = [];
+
+function logRequests(request, response, next) {
+  const { method, url } = request;
+
+  const logLabel = `[${method} ${url}]`;
+
+  console.time(logLabel);
+
+  next();
+
+  console.timeEnd(logLabel);
+}
+
+function validateProjectId(request, response, next) {
+  const { id } = request.params;
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: 'Invalid project ID.' });
+  }
+
+  return next();
+}
+
+app.use(logRequests)
+app.use('/projects/:id', validateProjectId);
 
 app.get('/projects', (request, response) => {
   const { title } = request.query;
