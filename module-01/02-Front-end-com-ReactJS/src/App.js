9bfa5627b01873d996df9e2721fef5fd65f7aa44
Victor Gianelli <victorgnll@gmail.com>
2020-10-04 21:10:06 -0300

    Listando Projetos da API

--- module-01/02-Front-end-com-ReactJS/src/App.js
+++ module-01/02-Front-end-com-ReactJS/src/App.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import api from './services/api'
 
 import './App.css'
-import backgroundImage from './assets/karsten.jpg'
 
 import Header from './components/Header'
 
@@ -12,21 +12,26 @@ import Header from './components/Header'
 */
 
 function App() {
-  const [projects, setProjects] = useState(['Desenvolvimento do app', 'Back-end web'])
+  const [projects, setProjects] = useState([])
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+      console.log(response)
+      setProjects(response.data)   
+    })
+  }, [])
 
   function handleAddProject() {
     setProjects([...projects, `Novo projeto ${Date.now()}`])
-    
+
     console.log(projects)
   }
   return (
     <>
       <Header title="Projects" />
 
-      <img height={500} src={backgroundImage} alt="karsten"/> 
-
       <ul>
-        {projects.map(project => <li key={project} >{project}</li>)}
+        {projects.map(project => <li key={project.id} >{project.title}</li>)}
       </ul>
 
       <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
