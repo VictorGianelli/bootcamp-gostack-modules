f3ed20a856eaf7a8a2588500302861148848709d
Victor Gianelli <victorgnll@gmail.com>
2020-10-04 21:23:11 -0300

    Cadastrando Projetos

--- module-01/02-Front-end-com-ReactJS/src/App.js
+++ module-01/02-Front-end-com-ReactJS/src/App.js
@@ -21,10 +21,17 @@ function App() {
     })
   }, [])
 
-  function handleAddProject() {
-    setProjects([...projects, `Novo projeto ${Date.now()}`])
+  async function handleAddProject() {
+    // setProjects([...projects, `Novo projeto ${Date.now()}`])
 
-    console.log(projects)
+    const response = await api.post('projects',{
+      title: `Novo projeto ${Date.now()}`, 
+      owner: "Victor"
+    })
+
+    const project = response.data
+
+    setProjects([...projects,project])
   }
   return (
     <>
