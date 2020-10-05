a03a48ed842cb33d9a2f3fecad0cbcdd24347521
Victor Gianelli <victorgnll@gmail.com>
2020-10-05 11:26:29 -0300

    Tipando objetos e vetores

--- module-01/04-TypeScript/src/routes.ts
+++ module-01/04-TypeScript/src/routes.ts
@@ -1,5 +1,17 @@
 import { Request, Response} from 'express'
+import createUser from './services/CreateUser'
 
 export function helloWorld(request: Request,response: Response){
+  const user = createUser({
+    email: 'victor@mail.com',
+    password: '123456',
+    techs: [
+      'Node.js',
+      'React Native',
+      'ReactJS',
+      { title: 'Java', experience: 85}
+    ]
+  });
+
   return response.json({ mensage : 'Hello World!'});
 }
