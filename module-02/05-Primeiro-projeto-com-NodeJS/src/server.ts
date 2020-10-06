3053de96987e54f4685405283a1ed5f6134d655e
Victor Gianelli <victorgnll@gmail.com>
2020-10-06 16:07:10 -0300

    Padrões com Eslint, Prettier e Editor Config

--- module-02/05-Primeiro-projeto-com-NodeJS/src/server.ts
+++ module-02/05-Primeiro-projeto-com-NodeJS/src/server.ts
@@ -1,11 +1,12 @@
-import express from 'express'
+import express from 'express';
+import routes from './routes';
 
-const app = express()
+const app = express();
 
 app.get('/', (request, response) => {
-  return response.json({ mensage : 'Hello World!'});
+  return response.json({ mensage: 'Hello World!' });
 });
 
 app.listen(3333, () => {
-  console.log('🚀 Back-end started')
+  console.log('🚀 Back-end started');
 });
