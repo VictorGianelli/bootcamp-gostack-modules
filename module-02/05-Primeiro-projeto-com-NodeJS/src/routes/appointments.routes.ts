34499ec70b7cc7766e3b57ce9d09c3af5c415df8
Victor Gianelli <victorgnll@gmail.com>
2020-10-06 21:47:20 -0300

    Criando repositórios

--- module-02/05-Primeiro-projeto-com-NodeJS/src/routes/appointments.routes.ts
+++ module-02/05-Primeiro-projeto-com-NodeJS/src/routes/appointments.routes.ts
@@ -1,29 +1,28 @@
 import { Router } from 'express';
 import { startOfHour, parseISO, isEqual } from 'date-fns';
+
 import Appointment from '../models/Appointment';
+import AppointmentsRepositoriy from '../repositories/AppoitmentsRepository';
 
 const appointmentsRouter = Router();
-
-const appointments: Appointment[] = [];
+const appointmentsRepositoriy = new AppointmentsRepositoriy();
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
   const parseDate = startOfHour(parseISO(date));
 
-  const findEventInTheSameDate = appointments.find(appointment =>
-    isEqual(parseDate, appointment.date),
+  const findAppoimtmentInTheSameDate = appointmentsRepositoriy.findByDate(
+    parseDate,
   );
 
-  if (findEventInTheSameDate) {
+  if (findAppoimtmentInTheSameDate) {
     return response
       .status(400)
       .json({ menssage: 'This appointment is already booked' });
   }
 
-  const appointment = new Appointment(provider, parseDate);
-
-  appointments.push(appointment);
+  const appointment = appointmentsRepositoriy.create(provider, parseDate);
 
   return response.json(appointment);
 });
