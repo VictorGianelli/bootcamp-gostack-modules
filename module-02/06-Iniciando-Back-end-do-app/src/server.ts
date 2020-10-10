f275b803ccf86e5d29a5f6059770b2d2a9008f23
Victor de Oliveira Gianelli <victorgnll@gmail.com>
2020-10-10 13:07:05 -0300

    Servindo arquivos estáticos

--- module-02/06-Iniciando-Back-end-do-app/src/server.ts
+++ module-02/06-Iniciando-Back-end-do-app/src/server.ts
@@ -2,17 +2,15 @@ import 'reflect-metadata';
 
 import express from 'express';
 import routes from './routes';
+import uploadConfig from './config/upload';
 
 import './database';
 
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
-
-app.get('/', (request, response) => {
-  return response.json({ mensage: 'Hello World!' });
-});
 
 app.listen(3333, () => {
   // eslint-disable-next-line no-console
