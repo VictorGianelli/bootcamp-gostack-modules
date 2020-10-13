2aaa1130ef7958e04502116a314a33622c2e55d0
Victor de Oliveira Gianelli <victorgnll@gmail.com>
2020-10-13 16:47:22 -0300

    Criando tooltip de erros

--- module-03/08-Iniciando front-end web/gobarber-web/src/components/Input/index.tsx
+++ module-03/08-Iniciando front-end web/gobarber-web/src/components/Input/index.tsx
@@ -1,8 +1,11 @@
 import React, { InputHTMLAttributes, useEffect, useRef, useState, useCallback } from 'react';
 import { IconBaseProps } from 'react-icons'
+import { FiAlertCircle } from 'react-icons/fi'
 import { useField } from '@unform/core'
 
-import { Container} from './styles'
+import Tooltip from '../Tooltip';
+
+import { Container, Error } from './styles'
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
@@ -23,7 +26,7 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
   const handleInputBlur = useCallback(() => {
     setIsFocused(false)
 
-     setIsFilled(!!inputRef.current?.value)
+    setIsFilled(!!inputRef.current?.value)
   }, [])
 
   useEffect(() => {
@@ -35,7 +38,7 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
   }, [fieldName, registerField])
 
   return (
-    <Container isFilled={isFilled} isFocused={isFocused}>
+    <Container isErrored={!!error} isFilled={isFilled} isFocused={isFocused}>
       { Icon && <Icon size={20} />}
       <input
         onFocus={handleInputFocus}
@@ -45,7 +48,10 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
         {...rest}
       />
 
-      {error}
+      {error && (
+      <Error title={error} >
+        <FiAlertCircle color="#c53030" size={20} />
+      </Error>)}
     </Container>
   )
 };
