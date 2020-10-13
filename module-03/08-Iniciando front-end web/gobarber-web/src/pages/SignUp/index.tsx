cb51716704616131ddf72b6d96570db28d07a4be
Victor de Oliveira Gianelli <victorgnll@gmail.com>
2020-10-13 16:07:41 -0300

    Validando cadastro

--- module-03/08-Iniciando front-end web/gobarber-web/src/pages/SignUp/index.tsx
+++ module-03/08-Iniciando front-end web/gobarber-web/src/pages/SignUp/index.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback } from 'react'
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi'
 import { Form } from '@unform/web'
+import * as Yup from 'yup'
 
 import logoImg from '../../assets/logo.svg';
 
@@ -12,7 +13,22 @@ import { Container, Content, Background } from './styles'
 const SingUp: React.FC = () => {
 
   const handleSubmit = useCallback(async (data: object) => {
-    console.log(data);
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string().required('E-mail obrigatório').email('Digite um e-mail válido'),
+        password: Yup.string().min(6, 'Mínimo 6 digitos')
+      })
+
+      await schema.validate(data,{
+        abortEarly: false,
+      })
+
+    } catch (err) {
+
+      console.log(err);
+
+    }
   },[])
 
   return (
