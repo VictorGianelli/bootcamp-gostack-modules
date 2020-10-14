c3de7a5e3b4c0ce41b05bb2466030dd17173dfb6
Victor de Oliveira Gianelli <victorgnll@gmail.com>
2020-10-13 21:13:21 -0300

    Mantendo usuário no storage

--- module-03/08-Iniciando front-end web/gobarber-web/src/pages/SignIn/index.tsx
+++ module-03/08-Iniciando front-end web/gobarber-web/src/pages/SignIn/index.tsx
@@ -20,9 +20,11 @@ interface SignInFormData {
 }
 
 const SingIn: React.FC = () => {
-  const formRef = useRef<FormHandles>(null)
+  const formRef = useRef<FormHandles>(null);
 
-  const { signIn } = useContext(AuthContext)
+  const { user, signIn } = useContext(AuthContext);
+
+  console.log(user);
 
   const handleSubmit = useCallback(
     async (data: SignInFormData) => {
