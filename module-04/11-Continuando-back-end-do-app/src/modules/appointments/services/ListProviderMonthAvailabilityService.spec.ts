3db730bdbbb82774f4c8de36349ccccd58e2c1d6
Victor de Oliveira Gianelli <victorgnll@gmail.com>
2020-11-03 08:29:28 -0300

    Filtrando agendamentos por mês

---
+++ module-04/11-Continuando-back-end-do-app/src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
@@ -0,0 +1,57 @@
+import AppError from '@shared/errors/AppError';
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import ListProvidersService from './ListProvidersService';
+import ListProviderMonthAvailabilityService from './ListProviderMonthAvailabilityService';
+
+let fakeAppointmentsReposirory: FakeAppointmentsRepository;
+let listProviderMonthAvailability: ListProviderMonthAvailabilityService
+
+describe('ListProviderMonthAvailabilityService', () => {
+  beforeEach(() => {
+    fakeAppointmentsReposirory = new FakeAppointmentsRepository();
+
+    listProviderMonthAvailability = new ListProviderMonthAvailabilityService(
+      fakeAppointmentsReposirory
+    )
+
+  })
+
+  it('should be able to list the the monrh avaliability from provider', async () => {
+
+    await fakeAppointmentsReposirory.create({
+      provider_id: 'user',
+      date: new Date(2020, 3, 20, 8, 0, 0),
+    })
+
+    await fakeAppointmentsReposirory.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 20, 8, 0, 0),
+    })
+
+    await fakeAppointmentsReposirory.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 20, 10, 0, 0),
+    })
+
+    await fakeAppointmentsReposirory.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 21, 8, 0, 0),
+    })
+
+    const avaliability = await listProviderMonthAvailability.execute({
+      provider_id: 'user',
+      year: 2020,
+      month: 5,
+    })
+
+    expect(avaliability).toEqual(
+      expect.arrayContaining([
+        { day: 19, available: true},
+        { day: 20, available: false},
+        { day: 21, available: false},
+        { day: 22, available: true},
+      ])
+    )
+  });
+
+})
---
+++ module-04/11-Continuando-back-end-do-app/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -0,0 +1,39 @@
+import { injectable, inject } from 'tsyringe';
+
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+
+import User from '@modules/users/infra/typeorm/entities/User';
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
+interface IRequest {
+  provider_id: string;
+  month: number;
+  year: number;
+}
+
+type IResponse = Array<{
+  day: number;
+  avaliable: boolean;
+}>
+
+@injectable()
+class ListProviderMonthAvailabilityService {
+  constructor(
+    @inject('appointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) { }
+
+  public async execute({ provider_id, month, year }: IRequest): Promise<IResponse> {
+    const appointments = await this.appointmentsRepository.findAllInMonthFromProvider({
+      provider_id,
+      year,
+      month
+    });
+
+    console.log(appointments);
+
+    return [{ day: 1, avaliable: false }];
+  }
+}
+
+export default ListProviderMonthAvailabilityService;
