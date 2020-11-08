7c81305322843020c3173d00c05f7b5d8a024eaa
Victor de Oliveira Gianelli <victorgnll@gmail.com>
2020-11-07 22:31:14 -0300

    Utilizando Class Transformer

--- module-04/12-Finalizando-back-end-do-app/src/modules/users/infra/http/controllers/ProfileController.ts
+++ module-04/12-Finalizando-back-end-do-app/src/modules/users/infra/http/controllers/ProfileController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import UpdateProfileService from '@modules/users/services/CreateUserService';
 import ShowProfileService from '@modules/users/services/ShowProfileService';
@@ -7,14 +8,14 @@ import ShowProfileService from '@modules/users/services/ShowProfileService';
 export default class ProfileController {
   public async show(request: Request, response: Response): Promise<Response> {
     const user_id = request.user.id;
-    
+
     const showProfile = container.resolve(ShowProfileService);
 
     const user = await showProfile.execute({ user_id })
-    
-    return response.json(user);
+
+    return response.json({ user: classToClass(user) });
   }
-  
+
   public async update(request: Request, response: Response): Promise<Response> {
     const user_id = request.user.id;
     const { name, email, old_password, password } = request.body;
@@ -22,16 +23,13 @@ export default class ProfileController {
     const updateProfile = container.resolve(UpdateProfileService);
 
     const user = await updateProfile.execute({
-      user_id,
+      // user_id,
       name,
       email,
-      old_password,
+      // old_password,
       password,
     });
 
-
-      delete user.password;
-
-      return response.json(user);
-    }
+    return response.json({ user: classToClass(user) });
+  }
 }
