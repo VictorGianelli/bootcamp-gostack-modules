b1401ce301650552ed2f561e26a25c814b24adfd
Victor de Oliveira Gianelli <victorgnll@gmail.com>
2020-11-09 13:21:28 -0300

    Emails pelo Amazon SES

--- module-04/12-Finalizando-back-end-do-app/src/shared/container/providers/index.ts
+++ module-04/12-Finalizando-back-end-do-app/src/shared/container/providers/index.ts
@@ -1,11 +1,12 @@
 import { container } from 'tsyringe';
+import mailConfig from '@config/mail';
 
 import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
+import SESMailProvider from './MailProvider/implementations/SESMailProvider';
 import IMailProvider from './MailProvider/models/IMailProvider';
 
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
-
 
 import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
 import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
@@ -22,5 +23,7 @@ container.registerInstance<IMailTemplateProvider>(
 
 container.registerInstance<IMailProvider>(
   'MailProvider',
-  container.resolve(EtherealMailProvider),
+  mailConfig.driver === 'ethereal' 
+  ? container.resolve(EtherealMailProvider)
+  : container.resolve(SESMailProvider),
 );
