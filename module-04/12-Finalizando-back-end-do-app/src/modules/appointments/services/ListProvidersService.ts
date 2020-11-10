efdf6d2997a0caee02867781605077898595c2e0
Victor de Oliveira Gianelli <victorgnll@gmail.com>
2020-11-10 10:32:47 -0300

    Cache lista de providers

--- module-04/12-Finalizando-back-end-do-app/src/modules/appointments/services/ListProvidersService.ts
+++ module-04/12-Finalizando-back-end-do-app/src/modules/appointments/services/ListProvidersService.ts
@@ -3,6 +3,7 @@ import { injectable, inject } from 'tsyringe';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 
 import User from '@modules/users/infra/typeorm/entities/User';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 interface IRequest {
   user_id: string;
@@ -13,12 +14,27 @@ class ListProvidersService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+    
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) { }
 
   public async execute({ user_id }: IRequest): Promise<User[]> {
-    const users = await this.usersRepository.findAllProviders({
+    let users = await this.cacheProvider.recover<User[]>(`providers-list:${user_id}`)
+    console.log('LPS')
+    if(!users) {
+      users = await this.usersRepository.findAllProviders({
       except_user_id: user_id,
     });
+
+    console.log('Query feita')
+
+    await this.cacheProvider.save(`providers-list:${user_id}`, users);
+
+    }
+    //console.log(cacheData); 
+
+    
 
     return users;
   }
