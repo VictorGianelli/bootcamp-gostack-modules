43e0c613c8fbb903191a973e693a3b28076a8e4d
Victor de Oliveira Gianelli <victorgnll@gmail.com>
2020-11-15 20:08:11 -0300

    Serialização no cache

--- module-04/12-Finalizando-back-end-do-app/src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ module-04/12-Finalizando-back-end-do-app/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,8 +1,9 @@
-import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
-import { inject, injectable } from 'tsyringe';
-import Appointment from '../infra/typeorm/entities/Appointment';
-import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+import { injectable, inject } from 'tsyringe';
 
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+import Appointment from '../infra/typeorm/entities/Appointment';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+import { classToClass } from 'class-transformer';
 
 interface IRequest {
   provider_id: string;
@@ -43,7 +44,7 @@ class ListProviderAppointmentsService {
 
       await this.cacheProvider.save(
         cacheKey,
-        appointments
+        classToClass(appointments)
       );
     }
 
