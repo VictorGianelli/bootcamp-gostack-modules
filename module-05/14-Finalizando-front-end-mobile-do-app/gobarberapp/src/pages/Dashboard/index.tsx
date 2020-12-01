79a3fed762f6b51cecf833661bbf916772791c9e
Victor de Oliveira Gianelli <victorgnll@gmail.com>
2020-12-01 17:03:48 -0300

    Listagem de prestadores

--- module-05/14-Finalizando-front-end-mobile-do-app/gobarberapp/src/pages/Dashboard/index.tsx
+++ module-05/14-Finalizando-front-end-mobile-do-app/gobarberapp/src/pages/Dashboard/index.tsx
@@ -1,10 +1,9 @@
 import { useNavigation } from '@react-navigation/native';
 import React, { useCallback, useEffect, useState } from 'react';
-import { View } from 'react-native';
 
+import Icon from 'react-native-vector-icons/Feather';
 import { useAuth } from '../../hooks/auth';
 import api from '../../services/api';
-import Profile from '../Profile';
 
 import {
   Container,
@@ -14,6 +13,13 @@ import {
   ProfileButton,
   UserAvatar,
   ProvidersList,
+  ProviderContainer,
+  ProviderAvatar,
+  ProviderInfo,
+  ProviderName,
+  ProviderMeta,
+  ProviderMetaText,
+  ProvidersListTitle,
 } from './styles';
 
 export interface Provider {
@@ -39,11 +45,20 @@ const Dashboard: React.FC = () => {
     signOut();
   }, [signOut]);
 
+  const navigateToCreateAppointment = useCallback(
+    (provider_id: string) => {
+      navigate('CreateAppointment', { provider_id });
+    },
+    [navigate],
+  );
+
   return (
     <Container>
       <Header>
         <HeaderTitle>
-          Bem-vindo, {'\n'}
+          Bem-vindo,
+{' '}
+          {'\n'}
           <UserName>{user.name}</UserName>
         </HeaderTitle>
 
@@ -54,8 +69,31 @@ const Dashboard: React.FC = () => {
 
       <ProvidersList
         data={providers}
-        keyExtractor={(provider) => provider.id}
-        renderItem={({ item }) => <UserName>{item.name}</UserName>}
+        keyExtractor={provider => provider.id}
+        ListHeaderComponent={
+          <ProvidersListTitle>Cabeleireiros</ProvidersListTitle>
+        }
+        renderItem={({ item }) => (
+          <ProviderContainer
+            onPress={() => navigateToCreateAppointment(item.id)}
+          >
+            <ProviderAvatar source={{ uri: item.avatar_url }} />
+
+            <ProviderInfo>
+              <ProviderName>{item.name}</ProviderName>
+
+              <ProviderMeta>
+                <Icon name="calendar" size={14} color="#ff9000" />
+                <ProviderMetaText>Segunda à Sexta</ProviderMetaText>
+              </ProviderMeta>
+
+              <ProviderMeta>
+                <Icon name="clock" size={14} color="#ff9000" />
+                <ProviderMetaText>8H às 18H</ProviderMetaText>
+              </ProviderMeta>
+            </ProviderInfo>
+          </ProviderContainer>
+        )}
       />
     </Container>
   );
